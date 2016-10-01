744934d9f7a2918b91510ca1aa412db7d26f9be2
benjamin-musil <benjamin.musil@tamu.edu>
2016-10-01 17:34:03 -0500

    Added Android folder

---
+++ Android/WearableRecipe/app/src/main/java/com/tamuhack/wearablerecipe/DrinkTypeActivity.java
@@ -0,0 +1,13 @@
+package com.tamuhack.wearablerecipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_type);
+    }
+}
